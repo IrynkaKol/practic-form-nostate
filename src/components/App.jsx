02521cafa5467a1aa5ab6f3179501d0270ff9f9b
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-16 14:40:31 +0100

    create login form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
+import { Component } from 'react';
+import {GlobalStyle} from './App.styled'
+import {LoginForm} from './LoginForm/LoginForm'
+
+export class App extends Component {
+  render() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <GlobalStyle>
+      <LoginForm />
+    </GlobalStyle>
   );
 };
+}
---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,18 @@
+export const LoginForm = () => {
+    const handleSubmit = e => {
+        e.preventDefaulte();
+        console.log(e.target)
+    }
+  return (
+    <form autoComplete="off" onSubmit={handleSubmit}>
+      <label htmlFor="login">
+        Login
+        <input type="text" name="login" />
+      </label>
+      <label htmlFor='password'>Password
+      <input type="password" name="password" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
